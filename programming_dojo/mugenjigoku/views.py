303d8279985fd3090cd2d5b5905f33084becb122
1izard <guitar.kubittake@gmail.com>
2019-10-19 11:41:30 +0900

    Navigate index page

--- programming_dojo/mugenjigoku/views.py
+++ programming_dojo/mugenjigoku/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import *
+
+
+class IndexView(generic.ListView):
+    model = Wazamoku
+    template_name = 'mugenjigoku/index.html'
+    context_object_name = 'wazamoku_lst'
